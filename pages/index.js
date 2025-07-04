df4c54b38816c8908b53ecc182d13dac44200330
RoeiGlamDev <Roeiglam2@gmail.com>
2025-07-04 13:42:17 +0700

    Add pages/index.js

---
+++ pages/index.js
@@ -0,0 +1,11 @@
+import { VideoGrid } from '../components/VideoGrid';
+import { Hero } from '../components/Hero';
+
+export default function Home() {
+  return (
+    <>
+      <Hero />
+      <VideoGrid />
+    </>
+  );
+}
